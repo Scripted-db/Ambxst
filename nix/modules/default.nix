07a766fedc35b306abdb0a11d6d50177506600a0
Axenide <axenide@gmail.com>
2026-01-15 20:02:43 -0300

    flake prepare

--- nix/modules/default.nix
+++ nix/modules/default.nix
@@ -30,6 +30,7 @@ in {
     # Enable recommended services for full functionality
     services.upower.enable = lib.mkDefault true;
     services.power-profiles-daemon.enable = lib.mkDefault true;
+    programs.gpu-screen-recorder.enable = lib.mkDefault true;
     networking.networkmanager.enable = lib.mkDefault true;
   };
 }
